77423d5a941225f35ba4ab5c3233623caa0d3e45
Ahmed Sakka <ahmedsakka720@gmail.com>
2023-02-05 21:35:55 +0100

    update hardcoded expense data

--- src/App.js
+++ src/App.js
@@ -3,27 +3,7 @@ import React, { useState } from 'react';
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    title: 'Mouse',
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: 'e4',
-    title: 'New Desk',
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
+import DUMMY_EXPENSES from './Data/data';
 
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
---
+++ src/Data/data.js
@@ -0,0 +1,29 @@
+const EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Computer Mouse',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'Smart TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Repair',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'Gaming Desk',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+  {
+    id: 'e5',
+    title: 'Phone',
+    amount: 1050.68,
+    date: new Date(2020, 5, 12),
+  }
+];
+
+export default EXPENSES;
